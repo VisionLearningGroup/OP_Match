eb342d9c617d84f46798a0197984b2cdf218af32
ksaito-ut <keisaito@bu.edu>
2021-07-10 13:39:35 -0400

    remove unnecessary file

--- run_cifar100_slurm.sh
+++
@@ -1,9 +0,0 @@
-python main.py --dataset cifar100 --num-labeled $1 --num-super $2 --out $3 --arch wideresnet --lambda_oem 0.1 --lambda_socr 1.0 \
---batch-size 64 --lr 0.03 --expand-labels --seed 0 --opt_level O2 --amp --mu 2
-
-
-
-
-
-
-
